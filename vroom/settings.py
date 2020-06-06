a469a5992509875f28eb466086e409725ac724fb
romqrq <rjaalbuquerque@gmail.com>
2020-06-06 21:29:12 +0000

    Updated settings and url to accommodate cart app

--- vroom/settings.py
+++ vroom/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'accounts',
     'home',
     'cars',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 # 'django.template.context_processors.media'
+                'cart.contexts.cart_contexts'
             ],
         },
     },
