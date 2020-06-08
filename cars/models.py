f5aae5704940ff3f2c640905df74b186b56f28bd
romqrq <rjaalbuquerque@gmail.com>
2020-06-08 19:50:18 +0000

    Updated car model, base.html and migrated

--- cars/models.py
+++ cars/models.py
@@ -4,20 +4,15 @@ from django.db import models
 class Car(models.Model):
     """Class for the car object"""
 
-    TRANSMISSION_OPTIONS = ['Manual', 'Automatic']
-    FUEL_OPTIONS = ['Diesel', 'Full Electric', 'Gasoline', 'Hybrid', 'Other']
-    # PASSENGER_OPTIONS = [(i, i) for i in (1, 10+)]
-    # DOORS_OPTIONS = [(i, i) for i in (1, 6+)]
-
     price = models.DecimalField(max_digits=6, decimal_places=2)
     brand = models.CharField(max_length=30, default='')
     model = models.CharField(max_length=30, default='')
     year = models.CharField(max_length=4, default='')
     engine = models.CharField(max_length=10, default='')
-    # transmission = GET OPTIONS
-    # fuel = GET OPTIONS
-    # passengers = GET OPTIONS
-    # doors = GET OPTIONS
+    transmission = models.CharField(max_length=10, default='')
+    fuel = models.CharField(max_length=40, default='')
+    passengers = models.CharField(max_length=10, default='')
+    doors = models.CharField(max_length=10, default='')
     accessories = models.CharField(max_length=200, default='')
     city = models.CharField(max_length=10, default='')
     county = models.CharField(max_length=10, default='')
