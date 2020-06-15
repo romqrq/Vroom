ffaf88b41a786e1dab1fa1b7841079c83861f959
romqrq <rjaalbuquerque@gmail.com>
2020-06-15 17:29:32 +0000

    Updated links on base

--- search/views.py
+++ search/views.py
@@ -3,7 +3,11 @@ from cars.models import Car
 
 
 def do_search(request):
-    """"""
+    """
+    Function to generate search results on findcar page based
+    on the "cars" object built from the filter function applied
+    to the form named "q"
+    """
 
-    cars = Car.objects.filter(name__icontains=request.GET['q'])
+    cars = Car.objects.filter(brand__icontains=request.GET['q'])
     return render(request, 'findcar.html', {'cars': cars})
