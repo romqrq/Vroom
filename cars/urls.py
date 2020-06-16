54c2162b8370c0c770ed3712bdea9c0b9b12e82b
romqrq <rjaalbuquerque@gmail.com>
2020-06-16 20:54:57 +0000

    Created views and urls for search by class

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
-from .views import all_cars
+from .views import all_cars, custom_classic_only, luxury_only, supersport_only
 
 urlpatterns = [
-    url(r'^$', all_cars, name='cars')
+    url(r'^$', all_cars, name='cars'),
+    url(r'^custom_classic/$', custom_classic_only, name='custom_classic_only'),
+    url(r'^luxury/$', luxury_only, name='luxury_only'),
+    url(r'^supersport/$', supersport_only, name='supersport_only'),
+
 ]
