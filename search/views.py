71ec3a07fa67d436dc1ace7228e15152dc479fc4
romqrq <rjaalbuquerque@gmail.com>
2020-06-16 22:37:43 +0000

    Updated search views for search with different parameters

--- search/views.py
+++ search/views.py
@@ -6,8 +6,17 @@ def do_search(request):
     """
     Function to generate search results on findcar page based
     on the "cars" object built from the filter function applied
-    to the form named "q"
+    to the form named "search_value"
     """
+    search_key = request.GET['search_key']
 
-    cars = Car.objects.filter(brand__icontains=request.GET['q'])
+    if search_key == 'Brand':
+        cars = Car.objects.filter(brand__icontains=request.GET['search_value'])
+    if search_key == 'Model':
+        cars = Car.objects.filter(model__icontains=request.GET['search_value'])
+    if search_key == 'Year':
+        cars = Car.objects.filter(year__icontains=request.GET['search_value'])
+    if search_key == 'Location':
+        cars = Car.objects.filter(city__icontains=request.GET['search_value'])
+
     return render(request, 'findcar.html', {'cars': cars})
