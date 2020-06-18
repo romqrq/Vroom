b349ceb0065cbdb57ef229e2bb698b3b7b08a8f5
romqrq <rjaalbuquerque@gmail.com>
2020-06-18 23:14:22 +0000

    Updated urls file to include new view

--- cars/urls.py
+++ cars/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
-from .views import all_cars, custom_classic_only, luxury_only, supersport_only
+from .views import all_cars, custom_classic_only, luxury_only, supersport_only, car_register
 
 urlpatterns = [
     url(r'^$', all_cars, name='cars'),
     url(r'^custom_classic/$', custom_classic_only, name='custom_classic_only'),
     url(r'^luxury/$', luxury_only, name='luxury_only'),
     url(r'^supersport/$', supersport_only, name='supersport_only'),
+    url(r'^rent_my_car/$', car_register, name='car_register')
 
 ]
