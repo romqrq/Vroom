1c1f218450998a8461ba7d90e8045a783b63b22e
romqrq <rjaalbuquerque@gmail.com>
2020-06-20 23:36:27 +0000

    Updated urls and views for about page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import index_view
+from .views import index_view, about_view
 
 
 urlpatterns = [
     url(r'^$', index_view, name='index'),
+    url(r'^about/$', about_view, name='about_page'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index_view(request):
     """View that displays the index page"""
     return render(request, 'index.html')
+
+
+def about_view(request):
+    """View that displays the About page"""
+    return render(request, 'about.html')
