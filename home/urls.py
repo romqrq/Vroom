5f2f086b408e1d0f2d1fae23fae3402c18dfcdc7
romqrq <rjaalbuquerque@gmail.com>
2020-06-21 17:24:57 +0000

    Updated urls for new faqs and contact pages

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import index_view, about_view
+from .views import index_view, about_view, faqs_view, contact_view
 
 
 urlpatterns = [
-    # url(r'^$', index_view, name='index'),
     url(r'^about/$', about_view, name='about'),
+    url(r'^FAQs/$', faqs_view, name='faqs'),
+    url(r'^contact/$', contact_view, name='contact'),
 ]
