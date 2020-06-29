d34178eccd89596a1ea1ab428fbf3c41275883bb
romqrq <rjaalbuquerque@gmail.com>
2020-06-29 14:34:21 +0000

    Created checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -16,10 +16,15 @@ stripe.api_key = settings.STRIPE_SECRET
 def checkout(request):
     """View to display checkout with relevant information for user"""
 
+    stripe.api_key = settings.STRIPE_SECRET
+
     if request.method == "POST":
         # getting user and payment information posted
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
+        # print('------------------')
+        # print(order_form['stripe_id'])
+        # print(request.stripe_id)
 
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
@@ -34,30 +39,40 @@ def checkout(request):
 
                 car = get_object_or_404(Car, pk=id)
                 total += quantity * car.price
-
+                # print('------------------')
+                # print(quantity)
+                # print(type(quantity))
                 order_line_item = OrderLineItem(
                     order=order,
                     car=car,
-                    quantity=quantity
+                    rental_days=quantity
                 )
+                # print('------------------')
+                # print(quantity)
+                # print(type(quantity))
 
                 order_line_item.save()
 
             # Trying to create a charge with stripe based on the cart content
             try:
+                # print('------------------')
+                print('try')
+                # print(payment_form.cleaned_data['stripe_id'])
                 customer = stripe.Charge.create(
                     amount=int(total * 100),
                     currency="EUR",
                     description=request.user.email,
                     card=payment_form.cleaned_data['stripe_id']
                 )
+
+                # print(customer)
             except stripe.error.CardError:
                 messages.error(request, "Your card was declined!")
 
             if customer.paid:
                 messages.error(request, "You have successfully paid!")
                 request.session['cart'] = {}
-                return redirect(reverse('products'))
+                return redirect(reverse('index'))
             else:
                 messages.error(request, "Unable to take payment")
         # If one of the filled forms isn't valid
