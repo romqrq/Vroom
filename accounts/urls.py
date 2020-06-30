24ce4ec593265d3938ed467e77d2040d2ee0e061
romqrq <rjaalbuquerque@gmail.com>
2020-06-30 19:02:43 +0000

    Updated urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from . import urls_reset
-from .views import register, profile, visit_profile, logout, login
+from .views import register, profile, visit_profile, logout, login, edit_user_view
 
 urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^profile/$', profile, name='profile'),
+    url(r'^edit_profile/(?P<user_id>\d+)', edit_user_view, name='edit_user'),
     url(r'visit_profile/(?P<userid>\d+)', visit_profile, name='visit_profile'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
