7ece66212d3f8947bedd8a37e8d51776837e16d3
romqrq <rjaalbuquerque@gmail.com>
2020-07-03 20:04:19 +0000

    Added static and media folders back to gitignore

--- vroom/settings.py
+++ vroom/settings.py
@@ -153,7 +153,6 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
