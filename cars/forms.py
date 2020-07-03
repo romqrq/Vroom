63e159604205c238b5c200233cffc6cc407ba1c4
romqrq <rjaalbuquerque@gmail.com>
2020-07-03 15:33:02 +0000

    Updated fields engine and displacement on car forms

--- cars/forms.py
+++ cars/forms.py
@@ -8,71 +8,15 @@ from .models import Car
 
 class CarRegistrationForm(ModelForm):
 
-    # CAR_CLASS_CHOICES = [
-    #     ('Custom Classic', 'Custom Classic'),
-    #     ('Luxury', 'Luxury'),
-    #     ('Supersport', 'Supersport')
-    # ]
-    # YEAR_CHOICES = [(i, i) for i in range(1900, 2021)]
-    # TRANSMISSION_CHOICES = [
-    #     ('Automatic', 'Automatic'),
-    #     ('Manual', 'Manual')
-    # ]
-    # FUEL_CHOICES = [
-    #     ('Diesel', 'Diesel'),
-    #     ('Full Electric', 'Full Electric'),
-    #     ('Hybrid', 'Hybrid'),
-    #     ('Petrol', 'Petrol')
-    # ]
-    # PASSENGER_CHOICES = [(i, i) for i in range(1, 30)]
-    # DOORS_CHOICES = [(i, i) for i in range(1, 10)]
-    # ACCESSORIES_CHOICES = [
-    #     ('Basic', 'Basic'),
-    #     ('Standard', 'Standard'),
-    #     ('Custom', 'Custom')
-    # ]
-    # CITY_CHOICES = [
-    #     ('Dublin', 'Dublin'),
-    #     ('Cork', 'Cork'),
-    #     ('Galway', 'Galway')
-    # ]
-    # COUNTY_CHOICES = [
-    #     ('Dublin', 'Dublin'),
-    #     ('Cork', 'Cork'),
-    #     ('Galway', 'Galway')
-    # ]
-    # COUNTRY_CHOICES = [('Ireland', 'Ireland')]
-
-    # user_id = forms.CharField(widget=forms.HiddenInput())
-    # user_id = User.objects.create(user_id=request.user.id)
-    # car_class = forms.ChoiceField(choices=CAR_CLASS_CHOICES)
-    # price = forms.DecimalField(max_digits=6, decimal_places=2)
-    # brand = forms.CharField(max_length=30)
-    # model = forms.CharField(max_length=30)
-    # year = forms.ChoiceField(choices=YEAR_CHOICES)
-    # engine = forms.CharField(max_length=10)
-    # transmission = forms.ChoiceField(choices=TRANSMISSION_CHOICES)
-    # fuel = forms.ChoiceField(choices=FUEL_CHOICES)
-    # passengers = forms.ChoiceField(choices=PASSENGER_CHOICES)
-    # doors = forms.ChoiceField(choices=DOORS_CHOICES)
-    # accessories = forms.ChoiceField(choices=ACCESSORIES_CHOICES)
-    # city = forms.ChoiceField(choices=CITY_CHOICES)
-    # county = forms.ChoiceField(choices=COUNTY_CHOICES)
-    # county = forms.ChoiceField(choices=COUNTRY_CHOICES)
-    # image1 = forms.ImageField()
-    # image2 = forms.ImageField()
-    # image3 = forms.ImageField()
-    # image4 = forms.ImageField()
-    # image5 = forms.ImageField()
     guidelines = forms.CharField(widget=forms.Textarea)
 
     class Meta:
         model = Car
         fields = [
-            'car_class', 'price', 'brand', 'model', 'year', 'engine',
-            'transmission', 'fuel', 'passengers', 'doors',
-            'accessories', 'city', 'county', 'country', 'image1',
-            'image2', 'image3', 'image4', 'image5', 'guidelines'
+            'car_class', 'price', 'brand', 'model', 'year', 'engine_type',
+            'displacement', 'transmission', 'fuel', 'passengers', 'doors',
+            'accessories', 'city', 'county', 'country', 'image1', 'image2',
+            'image3', 'image4', 'image5', 'guidelines'
         ]
 
 
@@ -84,6 +28,20 @@ class CarUpdateForm(ModelForm):
         ('Supersport', 'Supersport')
     ]
     YEAR_CHOICES = [(i, i) for i in range(1900, 2021)]
+    ENGINE_TYPE_CHOICES = [
+        ('Inline 3', 'Inline 3'),
+        ('Inline 4', 'Inline 4'),
+        ('Inline 6', 'Inline 6'),
+        ('Boxer 4', 'Boxer 4'),
+        ('Boxer 6', 'Boxer 6'),
+        ('V6', 'V6'),
+        ('V8', 'V8'),
+        ('V10', 'V10'),
+        ('V12', 'V12'),
+        ('W10', 'W10'),
+        ('W12', 'W12'),
+        ('Other', 'Other')
+    ]
     TRANSMISSION_CHOICES = [
         ('Automatic', 'Automatic'),
         ('Manual', 'Manual')
@@ -116,12 +74,18 @@ class CarUpdateForm(ModelForm):
     brand = forms.CharField(required=False)
     model = forms.CharField(required=False)
     year = forms.ChoiceField(choices=YEAR_CHOICES, required=False)
-    engine = forms.CharField(required=False)
-    transmission = forms.ChoiceField(choices=TRANSMISSION_CHOICES, required=False)
+    engine_type = forms.ChoiceField(choices=ENGINE_TYPE_CHOICES,
+                                    required=False)
+    displacement = forms.DecimalField(max_digits=4, decimal_places=1,
+                                      required=False)
+    transmission = forms.ChoiceField(choices=TRANSMISSION_CHOICES,
+                                     required=False)
     fuel = forms.ChoiceField(choices=FUEL_CHOICES, required=False)
-    passengers = forms.DecimalField(max_digits=2, decimal_places=0, required=False)
+    passengers = forms.DecimalField(max_digits=2, decimal_places=0,
+                                    required=False)
     doors = forms.DecimalField(max_digits=2, decimal_places=0, required=False)
-    accessories = forms.ChoiceField(choices=ACCESSORIES_CHOICES, required=False)
+    accessories = forms.ChoiceField(choices=ACCESSORIES_CHOICES,
+                                    required=False)
     city = forms.ChoiceField(choices=CITY_CHOICES, required=False)
     county = forms.ChoiceField(choices=COUNTY_CHOICES, required=False)
     country = forms.ChoiceField(choices=COUNTRY_CHOICES, required=False)
@@ -135,8 +99,8 @@ class CarUpdateForm(ModelForm):
     class Meta:
         model = Car
         fields = [
-            'car_class', 'price', 'brand', 'model', 'year', 'engine',
-            'transmission', 'fuel', 'passengers', 'doors',
-            'accessories', 'city', 'county', 'country', 'image1',
-            'image2', 'image3', 'image4', 'image5', 'guidelines'
+            'car_class', 'price', 'brand', 'model', 'year', 'engine_type',
+            'displacement', 'transmission', 'fuel', 'passengers', 'doors',
+            'accessories', 'city', 'county', 'country', 'image1', 'image2',
+            'image3', 'image4', 'image5', 'guidelines'
         ]
