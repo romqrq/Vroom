dc71400ade8b588937ebc4ab0ec72ab8fb7a789a
romqrq <rjaalbuquerque@gmail.com>
2020-07-03 20:23:58 +0000

    Testing static url name as staticfiles

--- vroom/settings.py
+++ vroom/settings.py
@@ -153,8 +153,10 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
