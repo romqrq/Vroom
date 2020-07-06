99acdd823f9646738a8a9f436c108fa0c6f0ffdb
romqrq <rjaalbuquerque@gmail.com>
2020-07-06 14:56:05 +0000

    Updated all_cars url name

--- cars/urls.py
+++ cars/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from .views import all_cars, custom_classic_only, luxury_only, supersport_only, car_register, car_detail, car_edit_view, del_car
 
 urlpatterns = [
-    url(r'^$', all_cars, name='cars'),
+    url(r'^$', all_cars, name='all_cars'),
     url(r'^custom_classic/$', custom_classic_only, name='custom_classic_only'),
     url(r'^luxury/$', luxury_only, name='luxury_only'),
     url(r'^supersport/$', supersport_only, name='supersport_only'),
