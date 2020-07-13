41af962680284b5499af3b1af300c14b9504bff3
romqrq <rjaalbuquerque@gmail.com>
2020-07-13 20:49:43 +0000

    Created expressions to deal with the different product types and increased the amount of information in the cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,12 +1,13 @@
 from django.shortcuts import get_object_or_404
-from cars.models import Car
+from cars.models import Car, TrackDayAddon, InsuranceAddon, PrivateDriverAddon
 
 
 def cart_contents(request):
     """
     Ensures that the cart contents are available when rendering every page
+    Different products are manipulated to render back cohesive and relevant
+    information
     """
-
     cart = request.session.get('cart', {})
 
     cart_items = []
@@ -14,13 +15,41 @@ def cart_contents(request):
     item_count = 0
     partial_value = []
 
-    for id, quantity in cart.items():
-        car = get_object_or_404(Car, pk=id)
-        total_cart += quantity * car.price
-        item_total = quantity * car.price
-        item_count += 1
-        partial_value.append({'id': id, 'item_total': item_total})
-        cart_items.append({'id': id, 'quantity': quantity, 'car': car})
+    for item in cart:
+        if item == 'car':
+            id = cart['car']['item_id']
+            quantity = cart['car']['quantity']
+            instance = Car
+            item_type = 'car'
+        elif item == 'track_day':
+            id = cart['track_day']['item_id']
+            quantity = cart['track_day']['quantity']
+            instance = TrackDayAddon
+            item_type = 'track_day'
+        elif item == 'insurance':
+            id = cart['insurance']['item_id']
+            quantity = cart['insurance']['quantity']
+            instance = InsuranceAddon
+            item_type = 'insurance'
+        elif item == 'private_driver':
+            id = cart['private_driver']['item_id']
+            quantity = cart['private_driver']['quantity']
+            instance = PrivateDriverAddon
+            item_type = 'private_driver'
 
+        item = get_object_or_404(instance, pk=id)
+        total_cart += quantity * item.price
+        item_total = quantity * item.price
+        item_count += 1
+        partial_value.append({
+            'item_type': item_type,
+            'id': id,
+            'item_total': item_total
+            })
+        cart_items.append({
+            'item_type': item_type,
+            'id': id,
+            'quantity': quantity,
+            })
     return {'cart_items': cart_items, 'partial_value': partial_value,
             'total_cart': total_cart, 'item_count': item_count}
