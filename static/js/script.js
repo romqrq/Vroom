c014e1d01978be881064687f93119bd129533fc7
romqrq <rjaalbuquerque@gmail.com>
2020-07-14 16:42:48 +0000

    Removed comments and tidied code

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,13 @@
 $(document).ready(function() {
     $('.form-trigger').click(function(){
-        // filled_form = $(this).attr('name');
         targetdiv_id = $(this).attr('value');
         $('.form-box').not('.d-none').addClass('d-none');
         $(`#${targetdiv_id}`).removeClass('d-none');
-        // $(`#${filled_form}`).submit('d-none');
     });
     $(".addon-radio").click(function(){
         item_id = $(this).attr('id');
         addon_type = $(this).attr('value');
         $(`#${addon_type}`).attr('action', `/cart/add/${addon_type}/${item_id}`);
     });
-    // $( "#carRegForm").submit(function( event ) {
-    //     alert( "Handler for .submit() called." );
-    //     event.preventDefault();
-    // });
-    // $('#crfSubmitButton').click(function(){
-    //     $("#carRegForm").submit() 
-    // })
+
 });
