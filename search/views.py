d1f49ac0d7029fcd97f8c4722db5da0b26e0797d
romqrq <rjaalbuquerque@gmail.com>
2020-07-17 20:44:04 +0000

    Changed class on messages div

--- search/views.py
+++ search/views.py
@@ -19,7 +19,7 @@ def do_search(request):
         cars = Car.objects.filter(year__icontains=request.GET['search_value'])
     if search_key == 'Location':
         cars = Car.objects.filter(city__icontains=request.GET['search_value'])
-    
+
     if not cars:
         messages.error(request, "Sorry, there are no results for this search.")
 
