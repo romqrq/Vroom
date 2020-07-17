238697916a2b249b4e07ffc729b8363781135788
romqrq <rjaalbuquerque@gmail.com>
2020-07-17 15:50:23 +0000

    Script checks if field is required and won't trigger field check when going back through form

--- static/js/script.js
+++ static/js/script.js
@@ -7,20 +7,36 @@ $(document).ready(function() {
         let current_div = $(this).attr('name');
         let targetdiv_id = $(this).attr('value');
         let all_inputs = []
-        // Creating an array with the value of every input element inside the current div
-        $(`#${current_div} :input`).each(function(){
-            field_value = $(this).val()
-            all_inputs.push(field_value)
-            });
-        // Searching the array to make sure that there aren't empty fields left 
-        if( ($.inArray("", all_inputs) !== -1 ) || ($.inArray(null, all_inputs) !== -1) ) {
-            alert("Please fill all required fields.");
-        }
-        else{
+
+        //The button to go back to the previous part of the form won't trigger the field check
+        if($(this).hasClass("back-button")){
             $('.form-box').not('.d-none').addClass('d-none');
             $(`#${targetdiv_id}`).removeClass('d-none');
+        } 
+        else{
+            // Creating an array with the value of every input element inside the current div
+            $(`#${current_div} :input`).each(function(){
+                field_value = $(this).val();
+                field_id = $(this).attr('id');
+                if($(`[for=${field_id}]`).hasClass('requiredField')){
+                    all_inputs.push(field_value);
+                }
+                else{
+                    return false
+                }
+            })
+
+            // Searching the array to make sure that there aren't empty fields left 
+            if( ($.inArray("", all_inputs) !== -1 ) || ($.inArray(null, all_inputs) !== -1) ) {
+                alert("Please fill all required fields.");
+            }
+            else{
+                $('.form-box').not('.d-none').addClass('d-none');
+                $(`#${targetdiv_id}`).removeClass('d-none');
+            }
         }
     });
+
     // Function to generate form action url dinamically following user choices
     // item_id => Id of the chosen/clicked item - String
     // addon_type => Type of add on chosen - String
