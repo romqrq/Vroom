0f99dc6f013517f16ce74c97fc53ba6dcc2f916e
romqrq <rjaalbuquerque@gmail.com>
2020-07-18 20:49:55 +0000

    tests for accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .forms import UserLoginForm, UserRegistrationForm, EditUserForm, EditUserPasswordForm
 
-# Create your tests here.
+
+class UserRegistrationFormTest(TestCase):
+    """Test run against user registration form"""
+
+    def test_UserRegistrationForm_valid(self):
+        user = User.objects.create(
+            username='testuser', first_name='test', last_name='user',
+            email='tu@test.com', password='123test'
+            )
+        form = UserRegistrationForm(instance=user)
+        self.assertTrue(form.is_valid())
