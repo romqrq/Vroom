7873bd0a3395c7db84319154a84077c0a124c2ca
romqrq <rjaalbuquerque@gmail.com>
2020-07-19 19:26:26 +0000

    updated clean password method

--- accounts/forms.py
+++ accounts/forms.py
@@ -33,16 +33,25 @@ class UserRegistrationForm(UserCreationForm):
         return email
 
     def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-
-        if not password1 or not password2:
-            raise ValidationError("Password must not be empty")
-
-        if password1 != password2:
-            raise ValidationError("Passwords do not match")
-
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
         return password2
+    # def clean_password2(self):
+    #     password1 = self.cleaned_data.get('password1')
+    #     password2 = self.cleaned_data.get('password2')
+
+    #     if not password1 or not password2:
+    #         raise ValidationError("Password must not be empty")
+
+    #     if password1 != password2:
+    #         raise ValidationError("Passwords do not match")
+
+    #     return password2
 
 
 class EditUserForm(forms.Form):
