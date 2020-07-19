8897d619138132c4529387c371f761faff46e3ee
romqrq <rjaalbuquerque@gmail.com>
2020-07-19 16:15:35 +0000

    Added urls tests for search

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from search.views import do_search
 
-# Create your tests here.
+
+# URLS
+class TestSearchUrls(SimpleTestCase):
+
+    def test_search_url_is_resolved(self):
+        url = reverse('search')
+        self.assertEqual(resolve(url).func, do_search)
