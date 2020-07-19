dc75e959e67ec8dfbc11ac7b94c3da6639b7d809
romqrq <rjaalbuquerque@gmail.com>
2020-07-19 16:14:29 +0000

    Added urls and part of forms tests for cars

--- cars/tests.py
+++ cars/tests.py
@@ -26,40 +26,39 @@ class CarRegistrationFormTest(TestCase):
     """Test ran agains Car registration form"""
 
     def test_CarRegisterForm_valid(self):
-        user = User.objects.create(
-            username='testuser', first_name='test', last_name='user',
-            email='tu@test.com', password='123test'
-            )
-        car = Car.objects.create(
-            car_owner=user,
-            car_class='Luxury', price=20, brand='Fiat', model='Panda',
-            year=1992, track_day='Yes', engine_type='Inline 4',
-            displacement=1.2, transmission='Manual', fuel='Petrol',
-            passengers='5', doors=4, accessories='Basic', city="Dublin",
-            county='Dublin', country='Ireland',
-            image1='static/images/index/business.jpg',
-            guidelines='Guidelines Test'
-        )
-        form = CarRegistrationForm(instance=car)
+        # user = User.objects.create(
+        #     username='testuser', first_name='test', last_name='user',
+        #     email='tu@test.com', password='123test'
+        #     )
+        form = CarRegistrationForm(data={
+            # 'car_owner': user,
+            'car_class': 'Luxury', 'price': 20, 'brand': 'Fiat', 'model': 'Panda',
+            'year': 1992, 'track_day': 'Yes', 'engine_type': 'Inline 4',
+            'displacement': 1.2, 'transmission': 'Manual', 'fuel': 'Petrol',
+            'passengers': '5', 'doors': 4, 'accessories': 'Basic', 'city': "Dublin",
+            'county': 'Dublin', 'country': 'Ireland',
+            'image1': 'static/images/index/business.jpg',
+            'guidelines': 'Guidelines Test'
+        })
+        print(form)
         self.assertTrue(form.is_valid())
 
     def test_CarRegisterForm_invalid(self):
 
-        user = User.objects.create(
-            username='testuser', first_name='test', last_name='user',
-            email='tu@test.com', password='123test'
-            )
-        car = Car.objects.create(
-            car_owner=user,
-            car_class='Luxury', price=20, brand='Fiat', model='Panda',
-            year=1992, track_day='Yes', engine_type='Inline 4',
-            displacement=1.2, transmission='Manual', fuel='Petrol',
-            passengers='5', doors=4, accessories='Basic', city="Dublin",
-            county='Dublin', country='Ireland',
-            image1='static/images/index/business.jpg',
-            guidelines='Guidelines Test'
-        )
-        form = CarRegistrationForm(instance=car)
+        # user = User.objects.create(
+        #     username='testuser', first_name='test', last_name='user',
+        #     email='tu@test.com', password='123test'
+        #     )
+        form = CarRegistrationForm(data={
+            # 'car_owner': user,
+            'car_class': '', 'price': 20, 'brand': '', 'model': 'Panda',
+            'year': 1992, 'track_day': 'Yes', 'engine_type': 'Inline 4',
+            'displacement': 1.2, 'transmission': 'Manual', 'fuel': 'Petrol',
+            'passengers': '5', 'doors': 4, 'accessories': 'Basic', 'city': "Dublin",
+            'county': 'Dublin', 'country': 'Ireland',
+            'image1': 'static/images/index/business.jpg',
+            'guidelines': 'Guidelines Test'
+        })
         self.assertFalse(form.is_valid())
 
 
@@ -208,7 +207,7 @@ class PrivateDriverTests(TestCase):
 
 
 # URLS
-class TestUrls(SimpleTestCase):
+class TestCarsUrls(SimpleTestCase):
 
     def test_all_cars_url_is_resolved(self):
         url = reverse('all_cars')
