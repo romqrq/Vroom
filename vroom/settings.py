f27201a9f5193e32246ee0342e3afbb859aacd34
romqrq <rjaalbuquerque@gmail.com>
2020-07-27 22:52:29 +0000

    Updated css for cart, checkout, profile, edit car and edit profile

--- vroom/settings.py
+++ vroom/settings.py
@@ -204,7 +204,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'accounts.backends.EmailAuth'
-]
+# AUTHENTICATION_BACKENDS = [
+#     'django.contrib.auth.backends.ModelBackend',
+#     'accounts.backends.EmailAuth'
+# ]
